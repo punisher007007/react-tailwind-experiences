b8d679802ac3bb42a6554fda8f8d4c88f343ba87
Rohith kanchanapalli <98241736+punisher007007@users.noreply.github.com>
2023-12-16 16:18:45 -0500

    Navbar-1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
 
   return (
     <div className='absolute w-full flex justify-between p-4 items-center'>
-      <h1 className='text-white font-bold text-2xl z-20'>Experiences</h1>
+      <h1 className='text-white font-bold text-2xl z-20'>We Offer</h1>
       <HiMenuAlt3 onClick={handleNav} className='z-20 text-white cursor-pointer' size={25} />
       <div
         className={
@@ -24,11 +24,11 @@ const Navbar = () => {
         }
       >
         <ul className='flex flex-col fixed w-full h-full items-center justify-center'>
-          <li className='font-bold text-3xl p-8'>Home</li>
-          <li className='font-bold text-3xl p-8'>Destinations</li>
-          <li className='font-bold text-3xl p-8'>Reservations</li>
-          <li className='font-bold text-3xl p-8'>Amenities</li>
-          <li className='font-bold text-3xl p-8'>Rooms</li>
+          <li className='font-bold text-3xl p-8'>Macro Photography</li>
+          <li className='font-bold text-3xl p-8'>Micro Photography</li>
+          <li className='font-bold text-3xl p-8'>Cinematic Editing</li>
+          <li className='font-bold text-3xl p-8'>Presets/ Luts</li>
+          <li className='font-bold text-3xl p-8'>Cinematic Editing Training</li>
         </ul>
       </div>
     </div>

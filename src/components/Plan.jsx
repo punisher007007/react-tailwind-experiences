dd155a5287790f4e3c9bbfb674e08823811e0004
Rohith kanchanapalli <98241736+punisher007007@users.noreply.github.com>
2023-12-16 18:03:24 -0500

    Images 1-9

--- src/components/Plan.jsx
+++ src/components/Plan.jsx
@@ -7,27 +7,27 @@ const Plan = () => {
       <div className='grid grid-cols-2 grid-rows-6 h-[80vh]'>
         <img
           className='row-span-3 object-cover w-full h-full p-2'
-          src='https://images.unsplash.com/photo-1506929562872-bb421503ef21?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1368&q=80'
+          src='images/l1.jpg'
           alt='/'
         />
         <img
           className='row-span-2 object-cover w-full h-full p-2'
-          src='https://images.unsplash.com/photo-1454391304352-2bf4678b1a7a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'
+          src='images/l2.jpg'
           alt='/'
         />
         <img
           className='row-span-2 object-cover w-full h-full p-2'
-          src='https://images.unsplash.com/photo-1515238152791-8216bfdf89a7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1772&q=80'
+          src='images/l3.jpg'
           alt='/'
         />
         <img
           className='row-span-3 object-cover w-full h-full p-2'
-          src='https://images.unsplash.com/photo-1468413253725-0d5181091126?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+          src='images/l4.jpg'
           alt='/'
         />
         <img
           className='row-span-2 object-cover w-full h-full p-2'
-          src='https://images.unsplash.com/photo-1473186578172-c141e6798cf4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1673&q=80'
+          src='images/l5.jpg'
           alt='/'
         />
       </div>
